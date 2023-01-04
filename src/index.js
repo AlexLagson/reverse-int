314ad036c3c67247e3d2a521f16a59974efd2ce1
Alex <alexlagwick12@gmail.com>
2023-01-04 12:53:24 +0100

    init: upload task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let reversedArr = [];
+    let str = String(n);
+    let arr = str.split('');
+    
+
+    for(let i = str.length-1; i >= 0; i--) {
+        let temp = arr[i];
+        reversedArr.push(arr[i]);
+    }
+
+    return parseInt(reversedArr.join(''));
 }
